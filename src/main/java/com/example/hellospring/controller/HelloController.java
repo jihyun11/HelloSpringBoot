902c29ce4516ccc232d5a21ab8753b3443dc7a1f
jihyun <372491@naver.com>
2023-05-10 14:09:09 +0900

    helloController를 통해 thymleaf 문법으로 전달된 데이터 출력해 보기, json 방식으로 컨트롤러 작성해 보기

--- src/main/java/com/example/hellospring/controller/HelloController.java
+++ src/main/java/com/example/hellospring/controller/HelloController.java
@@ -4,14 +4,41 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
-    @GetMapping("hell-mvc")
+    @GetMapping("hello-mvc")
     public String helloMVC(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
-        return "hello-templete";
+        return "hello-templates";
 
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
