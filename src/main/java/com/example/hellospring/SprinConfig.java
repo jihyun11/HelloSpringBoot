66606596cd59b2cc37b2c1b74637393b6ccdd79c
jihyun <372491@naver.com>
2023-05-17 13:53:15 +0900

    h2 데이터베이스를 이용해 회원가입 (등록), 조회의 기능 구현하기

--- src/main/java/com/example/hellospring/SprinConfig.java
+++ src/main/java/com/example/hellospring/SprinConfig.java
@@ -1,21 +1,33 @@
 package com.example.hellospring;
 
+import com.example.hellospring.repository.JdbcTemplateMemberRepository;
 import com.example.hellospring.repository.MemberRepository;
 import com.example.hellospring.repository.MemoryMemberRepository;
 import com.example.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SprinConfig {
+    @Configuration
+    public class SpringConfig {
+        private final DataSource dataSource;
 
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
+        public SpringConfig(DataSource dataSource) {
+            this.dataSource = dataSource;
+        }
 
-    @Bean
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        @Bean
+        public MemberService memberService() {
+            return new MemberService(memberRepository());
+        }
+
+        @Bean
+        public MemberRepository memberRepository() {
+
+            return new JdbcTemplateMemberRepository(dataSource);
+        }
     }
 }
