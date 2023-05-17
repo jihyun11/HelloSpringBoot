18cc4991f7ad0d8b207c774dc6367ebe3fb83923
jihyun <372491@naver.com>
2023-05-17 13:54:16 +0900

    회원가입, 중복회원예외에 대한 테스트코드 작성, @Transactional 어노테이션으로 테스트코드 재사용 가능하게 함

--- src/test/java/com/example/hellospring/service/MemberServiceTest.java
+++ src/test/java/com/example/hellospring/service/MemberServiceTest.java
@@ -1,70 +1,66 @@
 package com.example.hellospring.service;
 
 import com.example.hellospring.domain.Member;
+import com.example.hellospring.repository.MemberRepository;
 import com.example.hellospring.repository.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
+@Transactional
 class MemberServiceTest {
+    //테스트코드에서는 DI를 필드 주입으로 하는 게 편함
 
-    MemberService memberService;
-    MemoryMemberRepository memberRepository;
+    @Autowired MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
 
-    @BeforeEach //memberservice 에 대한 DI
-    public void beforeEach() {
-        memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memberRepository);
-    }
+//    @BeforeEach //memberservice 에 대한 DI
+//    public void beforeEach() {
+//        memberRepository = new MemoryMemberRepository();
+//        memberService = new MemberService(memberRepository);
+//    }
 
-    @AfterEach
-    public void afterEach() {
-        memberRepository.clearStore();
-    }
+
+
+//    @AfterEach
+//    public void afterEach() {
+//        memberRepository.clearStore();
+//    }
 
 
     @Test
-    void 회원가입() {
-        //given
+    public void 회원가입() throws Exception {
+
+        //Given
         Member member = new Member();
-        member.setName("spring");
-
-        //when
+        member.setName("springgg");
+        //When
         Long saveId = memberService.join(member);
-
-        //then
-        Member findMember = memberService.findOne(saveId).get();
-        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
-
+        //Then
+        Member findMember = memberRepository.findById(saveId).get();
+        assertEquals(member.getName(), findMember.getName());
     }
 
     @Test
-    public void 중복_회원_예외() {
-        //given
+    public void 중복_회원_예외() throws Exception {
+        //Given
         Member member1 = new Member();
         member1.setName("spring");
-
         Member member2 = new Member();
         member2.setName("spring");
-
-        //when
+        //When
         memberService.join(member1);
-        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
-        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-        //then
-    }
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));//예외가 발생해야 한다. assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+    } }
 
-    @Test
-    void findMembers() {
-    }
-
-    @Test
-    void findOne() {
-    }
-}
