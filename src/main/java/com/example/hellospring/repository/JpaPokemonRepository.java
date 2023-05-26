48203c1e41c749fe87e68e7c9ce8efbb0c55e468
jihyun <372491@naver.com>
2023-05-26 22:28:41 +0900

    jpa 리포지토리 설정, pokemon.html input 형식 바꾸기

--- src/main/java/com/example/hellospring/repository/JpaPokemonRepository.java
+++ src/main/java/com/example/hellospring/repository/JpaPokemonRepository.java
@@ -1,17 +1,28 @@
 package com.example.hellospring.repository;
 
+import com.example.hellospring.domain.Member;
 import com.example.hellospring.domain.Pokemon;
+import jakarta.persistence.EntityManager;
 
 import java.util.List;
 
 public class JpaPokemonRepository implements PokemonRepository{
+
+    private final EntityManager em;
+
+    public JpaPokemonRepository(EntityManager em) {
+        this.em = em;
+    }
+
     @Override
     public Pokemon save(Pokemon pokemon) {
-        return null;
+        em.persist(pokemon);
+        return pokemon;
     }
 
     @Override
     public List<Pokemon> findAll() {
-        return null;
+        List<Pokemon> result = em.createQuery("select m from Member m", Pokemon.class).getResultList(); //객체를 대상으로 쿼리를 날림
+        return result;
     }
 }
